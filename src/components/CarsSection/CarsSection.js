fa37e40b3c845358c9291d8e2a59f0de91155f29
SR TAMIM <saifur.tamim@gmail.com>
2021-11-10 23:08:53 +0600

    cars page ready

--- src/components/CarsSection/CarsSection.js
+++ src/components/CarsSection/CarsSection.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import SingleCar from './SingleCar/SingleCar';
 
-const CarsSection = () => {
-    return (
-        <div>
-            all cars
-        </div>
+const CarsSection = ({ dataAmount }) => {
+    const [cars, setCars] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://cars-zone.herokuapp.com/cars/${dataAmount ? dataAmount : 'all'}`)
+            .then(({ data }) => setCars(data))
+            .catch(err => console.log(err));
+    }, [dataAmount])
+
+
+    return (cars &&
+        <Box sx={{ maxWidth: 'xl', mx: 'auto', p: 2 }}>
+            <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {
+                    cars.map(carInfo => <SingleCar carInfo={carInfo} />)
+                }
+            </Grid>
+        </Box>
     );
 };
 
---
+++ src/components/CarsSection/SingleCar/SingleCar.js
@@ -0,0 +1,79 @@
+import { Grid, Typography } from '@mui/material';
+import { alpha, Box, styled } from '@mui/system';
+import React from 'react';
+
+
+const Icon = styled('i')(({ theme }) => ({
+    color: theme.palette.primary.light,
+    fontSize: '22px'
+}));
+const CarPrice = styled('h5')(({ theme }) => ({
+    background: alpha(theme.palette.primary.dark, 0.6),
+    color: 'white',
+    fontSize: '20px',
+    margin: '0 0 10px',
+    padding: '10px 20px',
+    borderRadius: '20px'
+}));
+
+
+const SingleCar = ({ carInfo }) => {
+    const { carImg, carName, carType, transmission, fuel, color, bodyStyle, mileage, price, engine } = carInfo;
+    return (
+        <Grid item xs={12} md={6} lg={4}>
+            <Box sx={{
+                overflow: 'hidden', height: '500px', position: 'relative',
+                display: 'flex', alignItems: 'flex-end'
+            }}>
+                <Box component="img" src={carImg} sx={{
+                    width: '100%',
+                }} ></Box>
+                <Box sx={{
+                    position: 'absolute', bottom: 0, left: 0, width: '100%',
+                    background: 'linear-gradient(#00000000,#00000077,#00000099)',
+                    color: 'white',
+                    px: 2, pt: 12, pb: 3
+                }} >
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography variant="h4" sx={{ fontWeight: 'bold' }}>{carName}</Typography>
+                        <CarPrice>${price}</CarPrice>
+                    </Box>
+                    <Box sx={{
+                        display: 'flex', flexWrap: 'wrap',
+                    }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-car"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{carType}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-tachometer-alt"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{transmission}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-gas-pump"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{fuel}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-palette"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{color}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-road"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{mileage}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-cogs"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{engine}</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mx: 1, mt: 1.5 }}>
+                            <Icon className="fas fa-tint"></Icon>
+                            <Typography variant="p" sx={{ textTransform: 'capitalize', mx: 1 }}>{bodyStyle}</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Grid>
+    );
+};
+
+export default SingleCar;
